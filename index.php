1911fb65b9f66b5596d0d824297297070dd33f00
Julian Hook <cirligiuilan12312@gmail.com>
2018-01-13 22:37:41 +0200

    julianhook

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Julian Hook</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1 class="message-coming">Julian Hook Website <br> is coming soon !</h1>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/89/three.min.js"></script>
+<script src="https://threejs.org/examples/js/renderers/Projector.js"></script>
+<script src="https://threejs.org/examples/js/renderers/CanvasRenderer.js"></script>
+<script src="https://threejs.org/examples/js/libs/stats.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
